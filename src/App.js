ab251382115d2834e4d51d286a65b02d6919c1bf
Cammy Smith <cjsarab1991@gmail.com>
2022-11-07 19:55:29 +0000

    Added container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import MainContainer from './containers/MainContainer';
 
 function App() {
   return (
-    <h1>Cammy is portfolio-ing!</h1>
+    <>
+    <h1>Cammy's Portfolio</h1>
+    <MainContainer />
+    </>
   );
-}
+};
 
 export default App;
