f7582794778f9117e6505840a7434cf61bcd3dd1
Cammy Smith <cjsarab1991@gmail.com>
2022-11-07 20:06:17 +0000

    Added basic components

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../components/Home';
+import Python from '../components/python/Python';
+import ErrorPage from '../components/ErrorPage';
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
+    <Router>
+        <>
+        <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/python" element={<Python />} />
+
+                <Route path="*" element={<ErrorPage />} />
+            </Routes>
+        </>
+    </Router>
   );
 };
 
